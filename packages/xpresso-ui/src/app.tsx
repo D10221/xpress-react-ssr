05a6b66b31242016b751791ed4d781d6a2397e43
d10221 <danieliozzi@gmail.com>
2018-08-21 05:24:51 +0200

    .

--- packages/xpresso-ui/src/app.tsx
+++ packages/xpresso-ui/src/app.tsx
@@ -1,40 +1,46 @@
-import * as React from 'react';
+import * as React from "react";
+import logo from "./logo.svg";
+import ssr from "./ssr";
 /** */
 class App extends React.Component {
   state = {
     mounted: false,
     logo: undefined as string | undefined,
     req: {}
-  }
+  };
 
-  componentDidMount() {
-    require("./index.css");
-    const req = require("./ssr").default();
+  async componentDidMount() {
+    require("./index.css");    
     this.setState({
       mounted: true,
-      logo: require('./logo.svg'),
-      req
+      logo,
+      req: ssr()
     });
   }
+  
   public render() {
     const { logo, mounted, req } = this.state;
     if (!mounted) {
-      return "... loading"
+      return "... loading";
     }
-    return mounted && (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit ... and save to reload.
-        </p>
-        <div style={{ margin: "1rem", padding: "1rem", textAlign: "left" }}>
-          <div>SSR: </div>
-          <pre>{JSON.stringify(req, null, 2).replace(/(\{|\}|\,|\")/gi, "")}</pre>
+    return (
+      mounted && (
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to React</h1>
+          </header>
+          <p className="App-intro">
+            To get started, edit ... and save to reload.
+          </p>
+          <div style={{ margin: "1rem", padding: "1rem", textAlign: "left" }}>
+            <div>SSR: </div>
+            <pre>
+              {JSON.stringify(req, null, 2).replace(/(\{|\}|\,|\")/gi, "")}
+            </pre>
+          </div>
         </div>
-      </div>
+      )
     );
   }
 }
