50ad5a422fc4085330397b75a1a4715eb3816a42
d10221 <danieliozzi@gmail.com>
2018-08-22 07:47:41 +0200

    .

--- packages/xpresso/src/error-handler.ts
+++ packages/xpresso/src/error-handler.ts
@@ -6,7 +6,7 @@ const errorHandler: ErrorRequestHandler = function errorHandler(
   next
 ) {
   if (error instanceof Error) {
-    return res.status((error as any).code || 500).send(error.toString());
+    return res.status(Number((error as any).code) || 500).send(error.toString());
   }
   return next(error);
 };
