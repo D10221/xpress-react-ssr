2f11ad20f2a1dab21ea521c51562fc8477fda930
me <danieliozzi@gmail.com>
2018-08-24 08:23:59 +0200

    ...

--- packages/xpresso/server/error-handler.ts
+++ packages/xpresso/server/error-handler.ts
@@ -1,31 +1,29 @@
 import { ErrorRequestHandler, Request } from "express";
 import { AuthError } from "@local/tiny-auth";
 import { JsonWebTokenError } from "jsonwebtoken";
-
+/** */
 function isApi(req: Request) {
   return /^\/api\/.*/.test(req.path);
 }
-/**
- *
- */
-export default function ErrorHandler(
-  isApiCall: (req: Request) => boolean = isApi
-): ErrorRequestHandler {
+/** */
+export type IsApiCall = (req: Request) => boolean;
+/** */
+export default function ErrorHandler(isApiCall: IsApiCall = isApi): ErrorRequestHandler {
+  /** */
   return function errorHandler(error, req, res, next) {
-    if (error instanceof AuthError) {
-      if (!isApiCall(req)) res.redirect("/login");
+    if (!isApiCall(req)) {
+      if (error instanceof AuthError) {
+        return res.redirect("/login");
+      }
+      if (error instanceof JsonWebTokenError) {
+        return res.redirect("/login");
+      }
     }
-
-    if (error instanceof JsonWebTokenError) {
-      if (!isApiCall(req)) res.redirect("/login");
-    }
-
     if (error instanceof Error) {
       return res
         .status(Number((error as any).code) || 500)
         .send(error.toString());
     }
-
     return next(error);
   };
 }
