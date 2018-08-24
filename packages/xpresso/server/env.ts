a6b1ba2a9764246642b8a9c2552962fecc4e7bca
d10221 <danieliozzi@gmail.com>
2018-08-24 06:49:47 +0200

    .

--- packages/xpresso/server/env.ts
+++ packages/xpresso/server/env.ts
@@ -1,7 +1,5 @@
 import env from "dotenv";
 import { join } from "path";
-const { PORT } = process.env;
 env.load({
   path: join(process.cwd(), ".env")
 });
-process.env.PORT = PORT || process.env.PORT || "5000";
