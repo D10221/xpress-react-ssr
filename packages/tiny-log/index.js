a0ae70d6901f9dc98d9b5ee77a07e1a0bfe05ecd
d10221 <danieliozzi@gmail.com>
2018-09-01 09:04:37 +0200

    tiny-log

--- packages/tiny-log/index.js
+++ packages/tiny-log/index.js
@@ -1,6 +1,6 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 /**
  * @param { typeof console.log } log;
- * @returns {{ info: typeof console.log, err: typeof console.log, warn: typeof console.log}}
+ * @returns {{ info: typeof console.log, err: typeof console.log, warn: typeof console.log, disabled: boolean}}
  */
 module.exports.default = require("./log");
--- packages/tiny-log/log.js
+++ packages/tiny-log/log.js
@@ -1,16 +1,23 @@
 const colors = require("ansi-colors");
 /**
  * @param { typeof console.log } log;
- * @returns {{ info: typeof console.log, err: typeof console.log, warn: typeof console.log}}
+ * @param options {{ devOnly: boolean}}
+ * @returns {{ info: typeof console.log, err: typeof console.log, warn: typeof console.log, disabled: boolean}}
  */
-module.exports = log => ({
-  info(message, ...args) {
-    return log(colors.blueBright(message), ...args);
-  },
-  err(message, ...args) {
-    return log(colors.redBright(message), ...args);
-  },
-  warn(message, ...args) {
-    return log(colors.yellowBright(message), ...args);
-  }
-});
+module.exports = (log, options) => {
+  const devOnly = options && !!options.devOnly;
+  const isDev = process.env.NODE_ENV !== "production";
+  const disabled = devOnly && !isDev;
+  return {
+    disabled,
+    info(message, ...args) {
+      !disabled && log(colors.blueBright(message), ...args);
+    },
+    err(message, ...args) {
+      !disabled && log(colors.redBright(message), ...args);
+    },
+    warn(message, ...args) {
+      !disabled && log(colors.yellowBright(message), ...args);
+    }
+  };
+};
