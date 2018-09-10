440ec6d3d685a1e7d3ff1ee0ae924fcfc641f081
me <danieliozzi@gmail.com>
2018-09-10 11:24:19 +0200

    redux-devtools

--- packages/react-ssr/src/store/index.ts
+++ packages/react-ssr/src/store/index.ts
@@ -1,4 +1,4 @@
-import { applyMiddleware, combineReducers, createStore, } from "redux";
+import { applyMiddleware, combineReducers, createStore, compose, } from "redux";
 import thunkMiddleware from "redux-thunk";
 import * as circuits from "../api/circuits/store";
 import * as session from "./session";
@@ -10,13 +10,25 @@ const reducer = combineReducers({
     [circuits.STORE_KEY]: circuits.reducer
 });
 
+/**
+   * dev-tools
+   */
+const enhance =
+    process.env.NODE_ENV === "production"
+        ? compose
+        : isWindow && (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+            ? (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+                // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+            })
+            : compose;
+
 const store = createStore(
     reducer,
     (isWindow && (window as any).REDUX_DATA) || {},
-    applyMiddleware(
+    enhance(applyMiddleware(
         thunkMiddleware,
         circuits.middleware
-    )
+    ))
 );
 
 /** */
